b165dae8e14da3daf8463bf7a03aa7a3831c7fb8
yehohanan12 <165852529+yehohanan12@users.noreply.github.com>
2025-06-22 23:05:56 +0100

    Set hero and best sellers text color to white

--- Frontend/src/Pages/HomePage.jsx
+++ Frontend/src/Pages/HomePage.jsx
@@ -13,13 +13,13 @@ export default function TestPage() {
         <div className="flex flex-col">
             {/* === SECTION HERO === */}
             <section className="bg-red-900 text-white text-center py-24 px-4">
-                <h1 className="text-4xl font-bold mb-4">DÉCOUVREZ VIB’IN</h1>
-                <p className="text-lg">L'harmonie parfaite entre musique et plaisir</p>
+                <h1 className="text-4xl font-bold mb-4 text-white">DÉCOUVREZ VIB’IN</h1>
+                <p className="text-lg text-white">L'harmonie parfaite entre musique et plaisir</p>
             </section>
 
             {/* === SECTION BEST SELLERS === */}
             <section className="py-16 px-4 text-center">
-                <h2 className="text-3xl font-semibold mb-8">BEST SELLERS</h2>
+                <h2 className="text-3xl font-semibold mb-8 text-white">BEST SELLERS</h2>
                 <div className="flex flex-wrap justify-center gap-6">
                     {PRODUCTS.map((p, index) => (
                         <div
